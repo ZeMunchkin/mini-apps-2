f4d8f355adddcd326c3ac1cec3eabcc551152bf6
Sara Brandes <smbrandes17@gmail.com>
2018-04-04 10:41:36 -0700

    init

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'client');
+
+const config = {
+  entry: `${APP_DIR}/app.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+module.exports = config;
